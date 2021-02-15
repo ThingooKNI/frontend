3760b426806c71d0fbfab8abbf1c35ca208970d6
Piotrek Brzeziński <thewildtree@outlook.com>
2021-02-15 20:38:25 +0100

    Add basic header elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './shared/header/header.component';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HeaderComponent } from './shared/header/header.component'
 import { FooterComponent } from './shared/footer/footer.component'
 
 @NgModule({
@@ -14,6 +16,8 @@ import { FooterComponent } from './shared/footer/footer.component'
     FooterComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
