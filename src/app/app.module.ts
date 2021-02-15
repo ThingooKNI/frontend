c86bcd9e5a9b7275de6c63029c8212eb0215a2c2
Piotrek Brzeziński <thewildtree@outlook.com>
2021-02-15 20:58:01 +0100

    Hook up basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,16 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HeaderComponent } from './shared/header/header.component'
 import { FooterComponent } from './shared/footer/footer.component'
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
     FooterComponent
   ],
   imports: [
