d887112c7dae122de77b4f9c8fc49aed8d53779e
Piotrek Brzeziński <thewildtree@outlook.com>
2021-02-18 16:22:02 +0100

    Make header aware of login state

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public isLoggedIn: boolean = false
 
-  constructor() { }
+  constructor(protected readonly keycloak: KeycloakService) { }
 
-  ngOnInit(): void {
+  public async ngOnInit() {
+    this.isLoggedIn = await this.keycloak.isLoggedIn()
+  }
+
+  public login() {
+    this.keycloak.login()
+  }
+
+  public logout() {
+    this.keycloak.logout()
   }
 
 }
