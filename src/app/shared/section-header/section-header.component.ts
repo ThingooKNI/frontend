d79ce92c7c17c74d813530cc97aff51fc3cc37a4
Piotrek Brzeziński <thewildtree@outlook.com>
2021-07-18 15:04:08 +0200

    Make compact header the default

--- src/app/shared/section-header/section-header.component.ts
+++ src/app/shared/section-header/section-header.component.ts
@@ -9,25 +9,10 @@ export class SectionHeaderComponent implements OnInit {
   @Input() iconName!: string;
   @Input() sectionTitle: string = '';
   @Input() sectionSubtitle: string = '';
-
-  private _compactRequested: boolean = false;
+  @Input() isExpanded: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  get isCompact() {
-    if (this._compactRequested) return true;
-    // Compact mode is on by default if there's no subtitle
-    if (this.sectionTitle && !this.sectionSubtitle)
-      return true;
-
-    return false;
-  }
-
-  @Input()
-  set isCompact(value: boolean) {
-    this._compactRequested = value;
   }
 }
