c457bc29308744c29be67ed4b3b504b3f83ddd37
Piotrek Brzeziński <thewildtree@outlook.com>
2021-07-18 14:36:16 +0200

    Add compact mode to SectionHeader

--- src/app/shared/section-header/section-header.component.ts
+++ src/app/shared/section-header/section-header.component.ts
@@ -8,10 +8,26 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SectionHeaderComponent implements OnInit {
   @Input() iconName!: string;
   @Input() sectionTitle: string = '';
+  @Input() sectionSubtitle: string = '';
+
+  private _compactRequested: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get isCompact() {
+    if (this._compactRequested) return true;
+    // Compact mode is on by default if there's no subtitle
+    if (this.sectionTitle && !this.sectionSubtitle)
+      return true;
+
+    return false;
+  }
+
+  @Input()
+  set isCompact(value: boolean) {
+    this._compactRequested = value;
+  }
 }
