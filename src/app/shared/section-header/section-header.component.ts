dc6a372446793e2d12a87c54368da32654cbff6a
Piotrek Brzeziński <thewildtree@outlook.com>
2021-07-18 14:12:19 +0200

    Add icon and (optional) title to SectionHeader

--- src/app/shared/section-header/section-header.component.ts
+++ src/app/shared/section-header/section-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
+  @Input() iconName!: string;
+  @Input() sectionTitle: string = '';
 
   constructor() { }
 
