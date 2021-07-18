cc1297951112f53d1dec2943ee4b780d0d0f9e37
Piotrek Brzeziński <thewildtree@outlook.com>
2021-07-18 14:55:06 +0200

    Update device list page

--- src/app/shared/device-card/device-card.component.ts
+++ src/app/shared/device-card/device-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Device } from 'src/app/api/device.model';
 
 @Component({
   selector: 'app-device-card',
   templateUrl: './device-card.component.html',
-  styleUrls: ['./device-card.component.scss'],
+  styleUrls: ['./device-card.component.scss']
 })
 export class DeviceCardComponent implements OnInit {
   @Input() device!: Device;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
 }
